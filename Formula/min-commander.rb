e193e95d20b5c39bac0905812ffd19e75a9f469a
devsternrassler <230136483+devsternrassler@users.noreply.github.com>
2026-05-18 05:02:42 +0000

    min-commander 2.1.3

--- Formula/min-commander.rb
+++ Formula/min-commander.rb
@@ -1,8 +1,8 @@
 class MinCommander < Formula
   desc "Keyboard-driven terminal file manager inspired by Norton Commander"
   homepage "https://github.com/sternrassler/commander-1"
-  url "https://github.com/sternrassler/commander-1/archive/refs/tags/v2.1.2.tar.gz"
-  sha256 "31b001586e8182ac2b899169bdb5405c41e1b5ec42b829ac1d79ac99d39312a7"
+  url "https://github.com/sternrassler/commander-1/archive/refs/tags/v2.1.3.tar.gz"
+  sha256 "9b26b9475d4dceefe0f5d50627c29e1411615ef8319a34f1235d1f9a57a22b0c"
   license "MIT"
   depends_on "go" => :build
 
